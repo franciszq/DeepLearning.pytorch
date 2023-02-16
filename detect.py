72b457c81f21f165e0c75deb4a3342f7ed969bee
calmisential <xinyu_std@163.com>
2023-02-16 19:42:58 +0800

    Add centernet to detect.py

--- detect.py
+++ detect.py
@@ -20,6 +20,22 @@ def parse_args():
     return args
 
 
+def detect_images(cfg, model_class, decode_fn, device):
+    """
+    检测多张图片中的目标
+    :param cfg: 配置信息
+    :param model_class:  网络模型的类名
+    :param decode_fn: 解码函数名
+    :param device: 设备
+    :return:
+    """
+    model = model_class(cfg).to(device)
+    model.load_state_dict(torch.load(WEIGHTS, map_location=device)["model"])
+    print(f"Loaded weights: {WEIGHTS}")
+    for img in IMAGE_PATHS:
+        decode_fn(cfg, model, img, print_on=True, save_result=True, device=device)
+
+
 def main():
     t0 = time.time()
     args = parse_args()
@@ -27,17 +43,9 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     if model_name == "ssd":
-        model = SSD(cfg).to(device)
-        model.load_state_dict(torch.load(WEIGHTS, map_location=device)["model"])
-        print(f"Loaded weights: {WEIGHTS}")
-        for img in IMAGE_PATHS:
-            ssd_decode.detect_one_image(cfg, model, img, print_on=True, save_result=True, device=device)
+        detect_images(cfg, SSD, ssd_decode.detect_one_image, device)
     elif model_name == "centernet":
-        model = CenterNet(cfg).to(device)
-        model.load_state_dict(torch.load(WEIGHTS, map_location=device)["model"])
-        print(f"Loaded weights: {WEIGHTS}")
-        for img in IMAGE_PATHS:
-            centernet_decode.detect_one_image(cfg, model, img, print_on=True, save_result=True, device=device)
+        detect_images(cfg, CenterNet, centernet_decode.detect_one_image, device)
 
     print(f"Total time: {(time.time() - t0):.2f}s")
 
