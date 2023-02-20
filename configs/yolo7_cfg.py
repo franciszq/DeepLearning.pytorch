02f8fbc83b4f9c2a8a2edc6a15af963e486f304c
calmisential <xinyu_std@163.com>
2023-02-20 19:05:36 +0800

    Update

---
+++ configs/yolo7_cfg.py
@@ -0,0 +1,85 @@
+
+class Config:
+    def __init__(self):
+        self.arch = self._Arch()
+        self.dataset = self._Dataset()
+        self.train = self._Train()
+        self.loss = self._Loss()
+        self.optimizer = self._Optimizer()
+        self.log = self._Log()
+        self.decode = self._Decode()
+
+    class _Arch:
+        def __init__(self):
+            # 目标类别数，与数据集有关，对于voc是20，对于coco是80
+            self.num_classes = 20
+            # 输入图片大小：(C, H, W)
+            self.input_size = (3, 640, 640)
+            self.depth = 1.00
+            self.width = 1.00
+            self.act = "silu"
+
+    class _Dataset:
+        # 数据集
+        def __init__(self):
+            # 数据集名称，"voc"或者"coco"
+            self.dataset_name = "voc"
+
+    class _Train:
+        # 训练参数
+        def __init__(self):
+            # 恢复训练时加载的checkpoint文件，""表示从epoch=0开始训练
+            # 测试时也需要在这里指定checkpoint文件
+            self.resume_training = ""
+            # 恢复训练时的上一次epoch是多少，-1表示从epoch=0开始训练
+            self.last_epoch = -1
+
+            self.epoch = 100
+            self.batch_size = 8
+            # 初始学习率
+            self.initial_lr = 1e-3
+            # warm up轮数
+            self.warmup_epochs = 0
+            self.milestones = []
+            self.gamma = 0.1
+
+            # 是否使用预训练权重
+            self.pretrained = False
+            # 模型保存间隔
+            self.save_interval = 5
+            # 每隔多少epoch在验证集上验证一次
+            self.eval_interval = 1
+            # 保存模型的文件夹
+            self.save_path = "saves"
+            # 是否启动tensorboard
+            self.tensorboard_on = True
+            # 是否使用混合精度训练
+            self.mixed_precision = True
+            # 多少个子进程用于数据加载
+            self.num_workers = 0
+            self.max_num_boxes = 30
+
+    class _Loss:
+        # 损失函数
+        def __init__(self):
+            self.ignore_threshold = 0.5
+
+    class _Optimizer:
+        # 优化器
+        def __init__(self):
+            self.name = "Adam"
+            self.scheduler_name = "None"
+
+    class _Log:
+        # 训练日志
+        def __init__(self):
+            # 日志文件保存文件夹
+            self.root = "out"
+            # 日志文件输出间隔
+            self.print_interval = 50
+
+    class _Decode:
+        def __init__(self):
+            self.test_results = "result"
+            self.conf_threshold = 0.5
+            self.nms_threshold = 0.65
