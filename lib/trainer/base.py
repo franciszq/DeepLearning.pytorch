ae7063753627d301f3c05cbaf5ef6735fa8adfed
calmisential <xinyu_std@163.com>
2023-04-10 17:17:30 +0800

    show_option默认为True

--- lib/trainer/base.py
+++ lib/trainer/base.py
@@ -107,7 +107,8 @@ class BaseTrainer:
         self.model_algorithm = None
 
         self.metric_names = []
-        self.show_option = []
+        # 默认为True
+        self.show_option = [True for _ in range(len(self.metric_names))]
 
         self.set_model_algorithm()
         self.load_data()
